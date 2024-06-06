70bfc30f0578d3746ed47a96e6d7ae7e850a8a9e
Sharad <sharadd19@gmail.com>
2024-06-06 19:16:14 +1000

    changed image location again

--- src/App.jsx
+++ src/App.jsx
@@ -55,7 +55,7 @@ function App() {
     <>
       <div className="wrapper">
         <div className="logo">
-          <img src="public/Memory Game Logo.svg" alt="" />
+          <img src="src/assets/images/Memory Game Logo.svg" alt="" />
         </div>
         {!gameOver && <Score score={score} bestScore={bestScore}></Score>}
         {(!gameOver) && 
